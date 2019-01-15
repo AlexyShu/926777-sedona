4a2796a1aab77122b6541949b80be0cf49c96a6d
alexy_shu <za9i@bk.ru>
2019-01-15 23:45:46 +0300

    добавила анимацию

--- js/js.js
+++ js/js.js
@@ -1,9 +1,9 @@
 var btn = document.querySelector(".hotels-search");
 var popup = document.querySelector(".hotels-search-form");
 
+
 btn.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.toggle("modal-show");
 });
-
 
