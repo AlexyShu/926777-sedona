665136973e6732f5aeb2d73bfba1963b6e95494d
alexy_shu <za9i@bk.ru>
2019-01-21 08:40:40 +0300

    внесла часть изменений по критериям

--- js/js.js
+++ js/js.js
@@ -7,12 +7,16 @@ var data2 = document.querySelector(".appointment-item-date2");
 btn.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.toggle("modal-show");
+});
+
+popup.addEventListener("animationend", function() {
+	popup.classList.remove("modal-show-animation");
 	popup.classList.remove("modal-error");
 });
 
 popup.addEventListener("submit", function(evt) {
-	if (!data1.value || !data2.value) {
-	evt.preventDefault();
-	popup.classList.add("modal-error");
+	if (!data1.value || !data2.value) { 
+	    evt.preventDefault(); 
+		popup.classList.add("modal-error");  	
    }
 });
